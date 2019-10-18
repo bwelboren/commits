b08b3f00f6a0464fb05e863c1512ed5b324fc08b
Bjorn <bjornwelboren@gmail.com>
2019-10-18 14:27:51 +0200

    Better style

--- commits.py
+++ commits.py
@@ -14,12 +14,14 @@ def get_user_commits(login, github_profile):
     # Filter by login
     r = requests.get(URL, {'author':login})
     r = r.json()
-    
+
     with open('CONTRIBUTORS.md', 'a') as contributor_file:
-        contributor_file.write(f"\n{login} {github_profile}\n")
+        contributor_file.write(f"<br/>{login} {github_profile}<br/>")
+        counter = 1
         for commits_by_user in r:
             commit_sha = commits_by_user['html_url'].split('/')[-1][:7]
-            contributor_file.write(f"* [{commit_sha}]({commits_by_user['html_url']})\n")
+            counter += 1
+            contributor_file.write(f"* [{commit_sha}]({commits_by_user['html_url']})<br/>")
 
     contributor_file.close()
 
